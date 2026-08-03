2f856eaaa6de06f697e09ec2167c6dc055bd2e74
priya mishra <163099060+codeacious-priya@users.noreply.github.com>
2026-08-03 11:32:22 +0530

    Time: 3 ms (78.95%) | Memory: 11.4 MB (48.19%) - LeetSync

--- 264-ugly-number-ii/ugly-number-ii.cpp
+++ 264-ugly-number-ii/ugly-number-ii.cpp
@@ -3,10 +3,10 @@ public:
     int nthUglyNumber(int n) {
         if(n==1) return 1;
 
-        vector<int>dp(n);
+        vector<int>dp(n+1);
         int i2=0,i3=0,i5=0;
         dp[0]=1;
-        for(int i=1;i<n;i++){
+        for(int i=1;i<=n;i++){
             dp[i]=min({dp[i2]*2,dp[i3]*3,dp[i5]*5});
 
             if(dp[i]==dp[i2]*2) i2++;
