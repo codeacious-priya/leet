35eeb8617eb77aa0e109da8769db8aa1f175fb31
priya mishra <163099060+codeacious-priya@users.noreply.github.com>
2026-08-03 18:00:07 +0530

    Time: 3 ms (40.94%) | Memory: 9.5 MB (30.57%) - LeetSync

---
+++ 205-isomorphic-strings/isomorphic-strings.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool isIsomorphic(string s, string t) {
+        unordered_map<char,char>mp1;
+        unordered_map<char,char>mp2;
+        int n=s.size();
+
+        for(int i=0;i<n;i++){
+            if(mp1.find(s[i])==mp1.end() && mp2.find(t[i])== mp2.end()){
+                mp1[s[i]]=t[i];
+                mp2[t[i]]=s[i];
+            }
+            else {
+               if( mp1[s[i]]!=t[i] || mp2[t[i]]!=s[i]){
+                return false;
+               }
+
+            }
+
+        }
+       return true; 
+    }
+};
