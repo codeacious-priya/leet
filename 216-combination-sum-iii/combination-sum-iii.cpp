44bc4965cdf4adac3743a9b881690b241c414116
priya mishra <163099060+codeacious-priya@users.noreply.github.com>
2026-08-15 01:45:13 +0530

    Time: 2 ms (18.96%) | Memory: 8.6 MB (82.97%) - LeetSync

--- 216-combination-sum-iii/combination-sum-iii.cpp
+++ 216-combination-sum-iii/combination-sum-iii.cpp
@@ -1,30 +1,40 @@
 class Solution {
 public:
-    void solve(int i,int k,int target,int arr[],vector<vector<int>>&ans,vector<int>temp){
-        if(target==0 && temp.size()==k ){
-            ans.push_back(temp);
+    void solve(int k,int target,int i,vector<int>&arr,vector<vector<int>>&ans,vector<int>&res){
+        
+        //base case
+        if(target==0 && k==0){
+            ans.push_back(res);
             return;
         }
-        if(i>=9){
+        if(k == 0 && target != 0){
             return;
         }
-        if(target<0) return;
-
-        if(arr[i]<=target){
-            temp.push_back(arr[i]);
-            solve(i+1,k,target-arr[i],arr,ans,temp);
-            temp.pop_back();
+        if(i==arr.size()){
+            return;
         }
-        solve(i+1,k,target,arr,ans,temp);
+        
+        if(target<0){
+            return;
+        }
+        
+        //take 
+       
+        res.push_back(arr[i]);
+        solve(k-1,target-arr[i],i+1,arr,ans,res);
+         res.pop_back();
+         
+        //not take
+       
+        solve(k,target,i+1,arr,ans,res);
     }
     vector<vector<int>> combinationSum3(int k, int n) {
-       int arr[9]={1,2,3,4,5,6,7,8,9};
-       vector<vector<int>>ans;
-       vector<int>temp;
+        vector<int>arr={1,2,3,4,5,6,7,8,9};
+        vector<vector<int>>ans;
+        vector<int>res;
 
-       solve(0,k,n,arr,ans,temp);
-       return ans;
-
+        solve(k,n,0,arr,ans,res);
+        return ans;
         
     }
 };
