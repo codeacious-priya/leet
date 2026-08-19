78d319f96af86a80bdf87b8e1d04dd6a3097160a
priya mishra <163099060+codeacious-priya@users.noreply.github.com>
2026-08-19 23:57:42 +0530

    Time: 22 ms (73.82%) | Memory: 60.2 MB (73.48%) - LeetSync

--- 1487-cinema-seat-allocation/cinema-seat-allocation.cpp
+++ 1487-cinema-seat-allocation/cinema-seat-allocation.cpp
@@ -2,26 +2,25 @@ class Solution {
 public:
 
 int maxNumberOfFamilies(int n, vector<vector<int>>& reservedSeats) {
-        unordered_map<int, unordered_set<int>> mp; //row -> seats in each row booked
+        unordered_map<int, int> mp; //row -> seats in each row booked
 
-        for(auto& reservedSeat : reservedSeats) {
+        for(auto& reservedSeat : reservedSeats) { //O(10^4)
             int row  = reservedSeat[0];
             int seat = reservedSeat[1];
 
-            mp[row].insert(seat);
+            mp[row] |= (1 << seat); //set bits are the booked seats
         }
 
         int result = (n - mp.size()) * 2;
 
-        for(auto& [row, bookedSeats] : mp) {
+        int maskA = (1 << 2) | (1 << 3) | (1 << 4) | (1 << 5); //set bits are the ones which I need empty for Group A
+        int maskB = (1 << 4) | (1 << 5) | (1 << 6) | (1 << 7); //set bits are the ones which I need empty for Group B
+        int maskC = (1 << 6) | (1 << 7) | (1 << 8) | (1 << 9); //set bits are the ones which I need empty for Group C
 
-            auto isAvailable = [&](int seat) {
-                return bookedSeats.find(seat) == bookedSeats.end();
-            };
-
-            bool graupA = isAvailable(2) && isAvailable(3) && isAvailable(4) & isAvailable(5);
-            bool graupB = isAvailable(4) && isAvailable(5) && isAvailable(6) & isAvailable(7);
-            bool graupC = isAvailable(6) && isAvailable(7) && isAvailable(8) & isAvailable(9);
+        for(auto& [row, bookedSeatsMask] : mp) { //min(10*n, 10^4)
+            bool graupA = (bookedSeatsMask & maskA) == 0;
+            bool graupB = (bookedSeatsMask & maskB) == 0;
+            bool graupC = (bookedSeatsMask & maskC) == 0;
 
             if(graupA && graupC)
                 result += 2;
