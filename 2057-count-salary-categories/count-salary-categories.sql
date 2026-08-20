c243036fdb3dd5136773227eee81814e8a078b46
priya mishra <163099060+codeacious-priya@users.noreply.github.com>
2026-08-20 19:39:35 +0530

    Time: 1762 ms (22.50%) | Memory: 0B (100.00%) - LeetSync

--- 2057-count-salary-categories/count-salary-categories.sql
+++ 2057-count-salary-categories/count-salary-categories.sql
@@ -1,15 +1,15 @@
 SELECT
     'Low Salary' As category
-    ,COUNT(CASE WHEN income<20000 THEN 1  end) AS accounts_count
+    ,sum(CASE WHEN income<20000 THEN 1 else 0 end) AS accounts_count
 FROM accounts
 union all
 select 
   'Average Salary' As category
-  ,count(case when income between 20000 and 50000 then 1  end) as accounts_count
+  ,sum(case when income between 20000 and 50000 then 1 else 0 end) as accounts_count
   from accounts
 
   union all
   select 
      'High Salary' As category 
-     ,count(case when income>50000 then 1  end) as accounts_count
+     ,sum(case when income>50000 then 1 else 0 end) as accounts_count
      from accounts
