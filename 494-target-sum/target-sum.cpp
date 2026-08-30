e05f365eb15004baefb45d1c6058a1cb8bdb64aa
priya mishra <163099060+codeacious-priya@users.noreply.github.com>
2026-08-30 15:47:07 +0530

    Time: 160 ms (31.70%) | Memory: 59.5 MB (8.73%) - LeetSync

--- 494-target-sum/target-sum.cpp
+++ 494-target-sum/target-sum.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    int solve(int i,int target,vector<int>&nums){
+    int solve(int i,int target,vector<int>&nums,vector<unordered_map<int,int>>&dp){
         // 
         if(i<0){
             if(target==0){
@@ -10,15 +10,19 @@ public:
                 return 0;
             }
         }
+        if(dp[i].count(target)){
+            return dp[i][target];
+        }
 
-        int add=solve(i-1,target-nums[i],nums);
-        int sub=solve(i-1,target+nums[i],nums);
+        int add=solve(i-1,target-nums[i],nums,dp);
+        int sub=solve(i-1,target+nums[i],nums,dp);
 
-        return add+sub;
+        return dp[i][target]=add+sub;
     }
     int findTargetSumWays(vector<int>& nums, int target) {
         int n=nums.size();
-        return solve(n-1,target,nums);
+        vector<unordered_map<int,int>>dp(n);
+        return solve(n-1,target,nums,dp);
         
     }
 };
