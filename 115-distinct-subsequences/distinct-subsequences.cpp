99876269e3eabcec368ca6bce0bcb668a39b9ecb
priya mishra <163099060+codeacious-priya@users.noreply.github.com>
2026-09-03 12:26:39 +0530

    Time: 31 ms (53.61%) | Memory: 44.2 MB (31.17%) - LeetSync

--- 115-distinct-subsequences/distinct-subsequences.cpp
+++ 115-distinct-subsequences/distinct-subsequences.cpp
@@ -1,27 +1,28 @@
 class Solution {
 public:
-    int solve(int i,int j,string s,string t,vector<vector<int>>&dp){
-        // base case
-        if(j==0) return 1;
-        if(i==0) return 0;
-       
-        if(dp[i][j]!=-1){
-            return dp[i][j];
-        }
-        if(s[i-1]==t[j-1]){
-           return  dp[i][j]= (solve(i-1,j-1,s,t,dp)+solve(i-1,j,s,t,dp));
-        }
-        else{
-          return  dp[i][j]= solve(i-1,j,s,t,dp);
-        }
-        
-    }
+   
     int numDistinct(string s, string t) {
         int n=s.size();
         int m=t.size();
-        vector<vector<int>>dp(n+1,vector<int>(m+1,-1));
+        vector<vector<double>>dp(n+1,vector<double>(m+1,0));
 
-        return solve(n,m,s,t,dp);
+        for(int i=0;i<n;i++){
+            dp[i][0]=1;
+        }
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+                if(s[i-1]==t[j-1]){
+                dp[i][j]= dp[i-1][j-1]+dp[i-1][j];
+                }
+                else{
+                  dp[i][j]= dp[i-1][j];
+                }
+            }
+        }
+
+
+
+        return dp[n][m];
         
     }
 };
