270bcf330b7fb1c1907804c865b7b4b28f3b3c8b
priya mishra <163099060+codeacious-priya@users.noreply.github.com>
2026-09-03 16:33:16 +0530

    Time: 67 ms (7.46%) | Memory: 84.4 MB (5.01%) - LeetSync

--- 72-edit-distance/edit-distance.cpp
+++ 72-edit-distance/edit-distance.cpp
@@ -1,10 +1,34 @@
 class Solution {
 public:
-   
+    int solve(int i,int j,string s1,string s2,vector<vector<int>>&dp){
+        // base case
+        //deletion->to ""
+        //insert->s2
+        if(i<0){
+            return j+1;
+        }
+        if(j<0){
+            return i+1;
+        }
+        if(dp[i][j]!=-1){
+            return dp[i][j];
+        }
+        if(s1[i]==s2[j]){
+           return dp[i][j]= solve(i-1,j-1,s1,s2,dp);
+        }
+        else{
+            int insert=1+solve(i-1,j,s1,s2,dp);
+            int del=1+solve(i,j-1,s1,s2,dp);
+            int rep=1+solve(i-1,j-1,s1,s2,dp);
+            return dp[i][j]= min({insert,del,rep});   
+        }
+    }
     int minDistance(string word1, string word2) {
         int n=word1.size();
         int m=word2.size();
-
+        vector<vector<int>>dp(n+1,vector<int>(m+1,-1));
+    return solve(n-1,m-1,word1,word2,dp);
+    /*
        vector<vector<long long>>dp(n+1,vector<long long>(m+1,0));
        // delete
        for(int i=1;i<=n;i++){
@@ -29,6 +53,6 @@ public:
             }
         }
        }
-        return dp[n][m];
+        return dp[n][m];*/
     }
 };
