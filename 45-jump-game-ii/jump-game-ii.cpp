2fae5abfaf81c839165fb0679b2855ab33c68650
priya mishra <163099060+codeacious-priya@users.noreply.github.com>
2026-09-06 18:33:42 +0530

    Time: 0 ms (100.00%) | Memory: 20.6 MB (78.52%) - LeetSync

--- 45-jump-game-ii/jump-game-ii.cpp
+++ 45-jump-game-ii/jump-game-ii.cpp
@@ -1,19 +1,25 @@
 class Solution {
 public:
-    int solve(vector<int>&nums , int i,vector<int>&dp){
-        int n = nums.size();
-        if(i==n-1)return 0;
-
-        if(dp[i] != -1)return dp[i];
-        int ans = n;
-        for(int j=1;j<=nums[i] && i+j<n;j++){
-            ans= min(ans, 1 + solve(nums , i+j, dp));
-        }
-        return dp[i] =  ans;
-    }
     int jump(vector<int>& nums) {
         int n = nums.size();
-        vector<int>dp(n+1,-1);
-        return solve(nums , 0, dp);
+        int max_idx = 0;
+        int cnt =1;
+        if(n==1)return 0;
+        int i = 0;
+        while(i<n){
+             int maxi  = i + nums[i];
+             if(maxi >= n-1)return cnt;
+             int j=i+1;
+             max_idx = i;
+             while(j <= maxi && j<n){
+                if(j+nums[j]> max_idx + nums[max_idx]){
+                    max_idx = j;
+                }
+                j++;
+             }
+             i = max_idx;
+             cnt++;
+        }
+        return -1;
     }
 };
