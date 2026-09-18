d88207d93b5f0ae8ee52e10a2e518d3f57d600e4
priya mishra <163099060+codeacious-priya@users.noreply.github.com>
2026-09-18 22:40:26 +0530

    Time: 11 ms (58.27%) | Memory: 41.3 MB (15.31%) - LeetSync

--- 494-target-sum/target-sum.cpp
+++ 494-target-sum/target-sum.cpp
@@ -1,41 +1,25 @@
 class Solution {
 public:
-   int solve(int i,int target,vector<int>&nums,vector<vector<int>>&dp){
-    //. 
-    if(i==0){
-        if(target==0 && nums[0]==0){
-            return 2;
-        }
-         if(target==0|| nums[0]==target){
-            return 1;
-        }
-        
-            return 0;
-        
-    }
-    if(dp[i][target]!=-1){
-        return dp[i][target];
-    }
-    int pick=0;
-    if(nums[i]<=target){
-        pick=solve(i-1,target-nums[i],nums,dp);
-    }
-
-    int not_pick=solve(i-1,target,nums,dp);
-
-    return dp[i][target]=pick+not_pick;
-   }
-    int findTargetSumWays(vector<int>& nums, int target) {
-        int sum=0;
-        for(auto it:nums){
-            sum+=it;
-        }
-        if((sum-target)%2!=0||(sum-target)<0){
-            return 0;
-        }
+    int solve(vector<int>&nums,int target,int sum,int i,vector<vector<int>>&dp){
+        // base case
         int n=nums.size();
-        int t=(sum-target)/2;
-        vector<vector<int>>dp(n,vector<int>(t+1,-1));
-        return solve(n-1,t,nums,dp);
+        if(sum==target && i==n) return 1;
+        if(i>=n) return 0;
+        if(sum!=0 && i>=n) return 0;
+
+        if(dp[i][sum+1000]!=-1) return dp[i][sum+1000];
+
+        int add=solve(nums,target,sum+nums[i],i+1,dp);
+        int sub=solve(nums,target,sum-nums[i],i+1,dp);
+        return dp[i][sum+1000]=add+sub;
+    }
+    int findTargetSumWays(vector<int>& nums, int target) {
+        int n=nums.size();
+        vector<vector<int>>dp(n,vector<int>(2005,-1));
+        
+        int ans =solve(nums,target,0,0,dp);
+        return ans;
+        
+        
     }
 };
