bd3ca0594d296c0b3c1268e87d152aff5a61e46e
priya mishra <163099060+codeacious-priya@users.noreply.github.com>
2026-09-18 22:57:47 +0530

    Time: 27 ms (66.95%) | Memory: 37.1 MB (24.34%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.cpp
+++ 518-coin-change-ii/coin-change-ii.cpp
@@ -1,43 +1,31 @@
 class Solution {
 public:
-    int solve(int i,int amount,vector<int>&coin,vector<vector<int>>&dp){
+    int solve(int i,int amount,vector<int>&coins,vector<vector<int>>&dp){
         // 
         if(i==0){
-            return amount%coin[0]==0;
-        }
-        if(dp[i][amount]!=-1){
-            return dp[i][amount];
-        }
+            if(amount%coins[i]==0){
+                return 1;
+            }
+            else{
+                return 0;
+            }
 
-        int not_pick=solve(i-1,amount,coin,dp);
-        int pick=0;
-        if(coin[i]<=amount){
-            pick=solve(i,amount-coin[i],coin,dp);
         }
+        if(i<0) return 0;
+        if(i<0 && amount!=0) return 0;
+
+        if(dp[i][amount]!=-1) return dp[i][amount];
+        int pick=0;
+        if(coins[i]<=amount){
+            pick=solve(i,amount-coins[i],coins,dp);
+        }
+        int not_pick=solve(i-1,amount,coins,dp);
         return dp[i][amount]=pick+not_pick;
     }
     int change(int amount, vector<int>& coins) {
         int n=coins.size();
-        vector<vector<unsigned long long>>dp(n,vector<unsigned long long>(amount+1,0));
-
-        for(int t=0;t<=amount;t++){
-            if(t%coins[0]==0)
-            dp[0][t]=1;
-            
-                
-            
-        }
-
-        for(int i=1;i<n;i++){
-            for(int t=0;t<=amount;t++){
-            unsigned   long long not_pick=dp[i-1][t];
-              unsigned  long long pick=0;
-                if(coins[i]<=t){
-                    pick=dp[i][t-coins[i]];
-                }
-                dp[i][t]=pick+not_pick;
-            }
-        }
-        return (int)dp[n-1][amount];
+        vector<vector<int>>dp(n+1,vector<int>(amount+10,-1));
+        return solve(n-1,amount,coins,dp);
+        
     }
 };
