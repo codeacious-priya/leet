15e540f53903181dc76d333fc6579b2712eda5c5
priya mishra <163099060+codeacious-priya@users.noreply.github.com>
2026-09-23 19:41:49 +0530

    Time: 0 ms (100.00%) | Memory: 22.3 MB (16.97%) - LeetSync

--- 735-asteroid-collision/asteroid-collision.cpp
+++ 735-asteroid-collision/asteroid-collision.cpp
@@ -5,29 +5,29 @@ public:
         stack<int>st;
 
         for(int i=0;i<n;i++){
-            while(!st.empty()&& st.top()>0 && asteroids[i]<0){
-                if(abs(st.top())<abs(asteroids[i])){
-                    st.pop();
-                    
-                }
-                else if(abs(st.top())>abs(asteroids[i])){
-                    asteroids[i]=0;
-                }
-                else{
-                    asteroids[i]=0;
-                    st.pop();
-                    break;
-                }
+           if(asteroids[i]>0){
+            st.push(asteroids[i]);
+           }
+           else{
+            while(!st.empty()&& st.top()<abs(asteroids[i])&& st.top()>0){
+                st.pop();
             }
-            if(asteroids[i]!=0){
+            if(!st.empty()&& st.top()==abs(asteroids[i])){
+               
+                st.pop();
+            }
+           else if(st.empty()||st.top()<0){
                 st.push(asteroids[i]);
             }
+           }
         }
         vector<int>ans(st.size());
         for(int i=st.size()-1;i>=0;i--){
-            ans[i]=st.top();
-            st.pop();
+           if(!st.empty()){
+               ans[i]=st.top();
+               st.pop();
+           }
         }
-        return ans; 
+        return ans;
     }
 };
